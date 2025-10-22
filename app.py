1d40bf5ca43a6107c4b447eff3d15c4eab32f756
dave <jesugbohunmi23@gmail.com>
2025-10-22 10:46:38 +0100

    Create initial project structure

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+db = {}
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Welcome to the String Analyzer API!"}), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
