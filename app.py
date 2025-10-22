922c529a7dffa2c85678988750737cc679d83277
dave <jesugbohunmi23@gmail.com>
2025-10-22 11:32:14 +0100

    Set up MongoDB connection

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-db = {}
+mongo_uri = os.getenv("MONGO_URI")
+db_name = os.getenv("DB_NAME")
 
-@app.route('/', methods=['GET'])
+client = MongoClient(mongo_uri)
+db = client[db_name]
+
+
+@app.route('/', methods=["GET"])
 def home():
     return jsonify({"message": "Welcome to the String Analyzer API!"}), 200
+
+@app.route('/test-db', methods=["GET"])
+def test_db():
+    try:
+        db.command("ping")
+        return jsonify({"message": "Database connection successful!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
